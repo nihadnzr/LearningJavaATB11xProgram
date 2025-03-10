bd306188b8fcbe225b0c2cab0486fa2e25939f02
hp <nihadnzr1@gmail.com>
2025-03-11 01:51:17 +0530

    11th March Pushing Code

---
+++ src/ex_01_Java_Basic/LAB002_CommentSection.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class LAB002_CommentSection {
+
+    public static void main(String[] args) {
+        System.out.println("Comment on line");
+//        This is test comment
+    }
+}
