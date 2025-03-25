08717d6320a5eaca17e494b671ffe06f52c039e4
hp <nihadnzr1@gmail.com>
2025-03-25 05:51:15 +0530

    21st March

---
+++ src/ex_10_DoWhileLoop/Lab01_DoWhileLoop.java
@@ -0,0 +1,11 @@
+package ex_10_DoWhileLoop;
+
+public class Lab01_DoWhileLoop {
+    public static void main(String[] args) {
+        int a = 0;
+        do{
+            System.out.println(a);
+            a++;
+        } while (a<0);
+    }
+}
