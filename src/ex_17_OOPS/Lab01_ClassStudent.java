1e6895e1f885b5fb0ca71ee20d6f146beb4f999e
hp <nihadnzr1@gmail.com>
2025-04-19 14:38:46 +0530

    19th April

---
+++ src/ex_17_OOPS/Lab01_ClassStudent.java
@@ -0,0 +1,14 @@
+package ex_17_OOPS;
+
+public class Lab01_ClassStudent {
+    public static void main(String[] args) {
+
+    }
+
+}
+class Student {
+    String name;
+    int age;
+
+    void sleep(){}
+}
